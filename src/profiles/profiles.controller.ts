d32cfc03bd5d92f296109dcd52bc82e12549430c
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-02-20 22:35:28 +0200

    find single test

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('profiles')
 export class ProfilesController {
   // GET /profiles
-
+  /* Challenge:
+    1. Grab the query parameter 'location' and return an array with one profile object with its only property/value being the location
+  */
   @Get()
-  findAll(@Query('age') age: number) {
-    return [{ age }];
+  findAll(@Query('location') location: string) {
+    return [{ location }];
   }
 
   // GET /profiles/:id
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
   // POST /profiles
   // PUT /profiles/:id
   // DELETE /profiles/:id
