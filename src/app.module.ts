7fa7a8190b8db481ba38ed15ea249391ec151134
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-02-20 21:38:13 +0200

    controller module generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
+import { ControllerModule } from './controller/controller.module';
 
 @Module({
-  imports: [ProfilesModule],
+  imports: [ProfilesModule, ControllerModule],
   controllers: [AppController],
   providers: [AppService],
 })
