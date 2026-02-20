5db5019e4b0a3f6a5b34e3af1f5aa06e04b6fe89
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-02-20 22:18:38 +0200

    test profile controller

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query } from '@nestjs/common';
+
+@Controller('profiles')
+export class ProfilesController {
+  // GET /profiles
+
+  @Get()
+  findAll(@Query('age') age: number) {
+    return [{ age }];
+  }
+
+  // GET /profiles/:id
+  // POST /profiles
+  // PUT /profiles/:id
+  // DELETE /profiles/:id
+}
